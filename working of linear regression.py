ddeb03b9587755427049668e17baa5a9447b4d3f
Mohanraj-80 <221501080@rajalakshmi.edu.in>
2023-09-30 14:30:42 +0530

    sucess

--- working of linear regression.py
+++ working of linear regression.py
@@ -1,3 +1,4 @@
+import math
 
 #Before starting the one of the most basic concept callled "Linear Regression(LR)" we can start with wonking of Linear_Regression just for understanding
 
@@ -9,18 +10,20 @@ import numpy as np
 def pred(x1,y1):
     m_curr, b_curr = 0,0
     ini = 0.0002
-    r = 415533
-
+    prev = 0
+    cost2 = 1
     n = len(x1)
-    for i in range(r):
+    i = 0
+    while(not(math.isclose(prev,cost2,rel_tol = 1e-10))):
+        i+=1
+        prev = cost2
         y_pred = m_curr*x1 + b_curr
         cost2 = (1/n)*sum(val**2 for val in (y1 - y_pred))
         md = -(2/n)*sum(x1*(y1 - y_pred))
         bd = -(2/n)*sum(y1 - y_pred)
         m_curr = m_curr - ini*md
         b_curr = b_curr - ini*bd
-        if(i%100 == 0):
-            print(f"The weight of {m_curr} and bias of {b_curr} with the cost of {cost2} in the iteration of {i}")
+        print(f"The weight of {m_curr} and bias of {b_curr} with the cost of {cost2} in the iteration of {i}")
     return [m_curr,b_curr]
 def calc(f,a):
 
@@ -30,11 +33,11 @@ def calc(f,a):
     y = np.array(y)
 
     m, b = pred(x, y)
-    predicted_ans = a*m + b
+    predicted_ans = math.floor(a*m + b)
     return predicted_ans
 
 file = pd.read_csv('marks.csv')
-predicted_value = calc(file,80)
+predicted_value = calc(file,92)
 print(f"When the math mark is 80 and :\nAuctal cs mark :83\npredicted cs mark {predicted_value}")
 
 
